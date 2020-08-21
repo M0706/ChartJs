6a9e00f3531bf3545d0e98416aaad6b49cf65430
Manav Garg <garg.6@iitj.ac.in>
2020-08-22 03:09:22 +0530

    added bootstrap Modal

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
 import {Bar, Line, Pie} from 'react-chartjs-2';
+import { Modal,Button } from 'react-bootstrap';
 
 class Chart extends Component{
   constructor(props){
     super(props);
     this.state = {
-      chartData:props.chartData
+      chartData:props.chartData,
+      show:false
     }
   }
+
+    handleHide = () => {
+      this.setState({ show: false });
+    }
+
+    zoom=()=>{
+      this.setState({ show: true });
+      alert("clicked")
+    }
 
   static defaultProps = {
     displayTitle:true,
@@ -16,9 +27,37 @@ class Chart extends Component{
     location:'City'
   }
 
+
   render(){
     return (
       <div className="chart">
+        <Button
+          bsStyle="primary"
+          bsSize="large"
+          onClick={() => this.zoom()}
+        >
+          ZOOM IN
+        </Button>
+
+        <Modal
+          show={this.state.show}
+          onHide={this.handleHide}
+          container={this}
+          aria-labelledby="contained-modal-title"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title">
+              Contained Modal
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            jsfdl
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleHide}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+
         <Bar
           data={this.state.chartData}
           height={100}
@@ -32,7 +71,7 @@ class Chart extends Component{
             legend:{
               display:this.props.displayLegend,
               position:this.props.legendPosition
-            },
+            }
           }}
         />
 
